b2a071f2df2f7454adee6fd635ea424b97ce3c65
adjonata <alexdjonata30@gmail.com>
2021-11-01 19:53:16 -0300

    feat: Header component

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="default">
+    <DHeader v-if="loggedIn" />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+
+export default Vue.extend({
+  computed: {
+    ...mapGetters('auth', ['loggedIn']),
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
