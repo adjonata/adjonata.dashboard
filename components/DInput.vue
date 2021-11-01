3073e927d047d15f9db3ef2a39a29e0c98feea3c
adjonata <alexdjonata30@gmail.com>
2021-11-01 18:27:10 -0300

    feat: DInput component

---
+++ components/DInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <label class="d-input">
+    {{ label }}
+    <input
+      :value="value"
+      :type="type"
+      v-bind="$attrs"
+      @input="$emit('input', $event.target.value)"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DInput',
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: 'Input',
+    },
+    type: {
+      type: String,
+      required: true,
+      default: 'text',
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.d-input {
+  display: flex;
+  flex-direction: column;
+  font-weight: 500;
+  margin: 5px 0;
+  text-align: left !important;
+
+  input {
+    margin-top: 4px;
+  }
+}
+</style>
