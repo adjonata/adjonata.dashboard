d06a030d1b499aa7b1c5341b0b23c8640c45518d
adjonata <alexdjonata30@gmail.com>
2021-11-03 01:29:06 -0300

    fix: remove color in section interface

--- utils/sections.ts
+++ utils/sections.ts
@@ -2,7 +2,6 @@ type Section = {
   title: string
   path: string
   icon: string
-  color: string
 }
 
 const sections: Section[] = [
@@ -10,31 +9,26 @@ const sections: Section[] = [
     title: 'Home',
     path: '/',
     icon: 'home',
-    color: '#eb521e',
   },
   {
     title: 'About',
     path: '/about',
     icon: 'help',
-    color: '#bf642c',
   },
   {
     title: 'Knowledges',
     path: '/knowledges',
     icon: 'account_balance',
-    color: '#2cbf76',
   },
   {
     title: 'Projects',
     path: '/projects',
     icon: 'build',
-    color: '#8539b8',
   },
   {
     title: 'Social',
     path: '/social',
     icon: 'public',
-    color: '#3976b8',
   },
 ]
 
