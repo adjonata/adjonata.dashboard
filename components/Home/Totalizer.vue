dfde604dcd201b5d6ed7e20b2ebab68331f4108f
adjonata <alexdjonata30@gmail.com>
2021-11-04 17:30:23 -0300

    feat: HomeTotalizer component

---
+++ components/Home/Totalizer.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="totalizer">
+    <h2>{{ count }}</h2>
+    <div class="totalizer__description">
+      <span class="material-icons" v-text="icon" />
+      <h4>{{ title }}</h4>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '~/styles/variables';
+
+.totalizer {
+  width: 150px;
+  padding: 15px;
+  margin: 0 20px 20px;
+  border-radius: 10px;
+  background: $blue-light;
+  box-shadow: $shadow;
+
+  h2 {
+    font-weight: 600;
+    font-size: 34px;
+  }
+
+  &__description {
+    display: flex;
+    align-items: center;
+    margin-top: 8px;
+
+    span {
+      color: $white-light;
+      padding-right: 5px;
+    }
+    h4 {
+      color: $white-light;
+    }
+  }
+}
+</style>
