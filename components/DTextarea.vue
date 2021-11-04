8deee064de536dc393403964d4d5ce6c6c9afaae
adjonata <alexdjonata30@gmail.com>
2021-11-04 16:17:32 -0300

    feat: DTextarea component

--- components/DTextarea.vue
+++ components/DTextarea.vue
@@ -1,18 +1,24 @@
+<template>
+  <label class="d-textarea">
+    {{ label ? label : null }}
+    <textarea
+      :value="value"
+      v-bind="$attrs"
+      @input="$emit('input', $event.target.value)"
+    />
+  </label>
+</template>
+
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'DInput',
+  name: 'DTextarea',
   props: {
     label: {
       type: String,
-      required: true,
-      default: 'Input',
-    },
-    type: {
-      type: String,
-      required: true,
-      default: 'text',
+      required: false,
+      default: '',
     },
     value: {
       type: String,
@@ -22,28 +28,19 @@ export default Vue.extend({
 })
 </script>
 
-<template>
-  <label class="d-input">
-    {{ label }}
-    <input
-      :value="value"
-      :type="type"
-      v-bind="$attrs"
-      @input="$emit('input', $event.target.value)"
-    />
-  </label>
-</template>
-
 <style lang="scss" scoped>
-.d-input {
+.d-textarea {
   display: flex;
   flex-direction: column;
   font-weight: 500;
   margin: 5px 0;
   text-align: left !important;
 
-  input {
+  textarea {
+    width: 100%;
     margin-top: 4px;
+    padding: 10px;
+    resize: vertical;
   }
 }
 </style>
