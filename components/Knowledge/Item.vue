743b1b59ced93f5a562411209638758e5f2786a1
adjonata <alexdjonata30@gmail.com>
2021-11-06 15:39:40 -0300

    feat: KnowledgeItem component

---
+++ components/Knowledge/Item.vue
@@ -0,0 +1,89 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    openLink() {
+      window.open(this.link, '_blank')
+    },
+  },
+})
+</script>
+
+<template>
+  <article class="knowledge-item">
+    <div class="knowledge-item__infos">
+      <img :src="image" :alt="title + '.png'" />
+      <h3
+        :class="{ link: !!link }"
+        @click="link ? openLink() : null"
+        v-text="title"
+      />
+    </div>
+    <div class="knowledge-item__actions">
+      <button class="small">
+        <span class="material-icons">edit</span>
+      </button>
+      <button class="small">
+        <span class="material-icons">delete</span>
+      </button>
+    </div>
+  </article>
+</template>
+
+<style scoped lang="scss">
+@import '~/styles/variables', '~/styles/flex';
+
+.knowledge-item {
+  padding: 20px 30px;
+  border-radius: 10px;
+  box-shadow: $shadow;
+  background: $white-light;
+
+  &__infos {
+    @extend .flex-column-center;
+    text-align: center;
+    padding: 5px 0;
+
+    img {
+      width: 100px;
+    }
+    h3 {
+      font-weight: 500;
+      margin: 15px 0 5px 0;
+    }
+  }
+
+  &__actions {
+    @extend .flex-row-center;
+
+    button {
+      &:nth-child(1) {
+        span {
+          color: $blue-light;
+        }
+      }
+      &:nth-child(2) {
+        span {
+          color: $grey-light;
+        }
+      }
+    }
+  }
+}
+</style>
