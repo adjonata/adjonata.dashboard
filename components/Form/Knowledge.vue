2fa1e39c2755a12a5de2fb349dea1d4d69718f6c
adjonata <alexdjonata30@gmail.com>
2021-11-06 16:22:49 -0300

    feat: KnowledgeForm

---
+++ components/Form/Knowledge.vue
@@ -0,0 +1,63 @@
+<script lang="ts">
+import Vue from 'vue'
+import ValidateKnowledge from '@/utils/knowledge/validate'
+
+export default Vue.extend({
+  props: {
+    initialValue: {
+      type: Object,
+      required: false,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    form: {
+      title: '',
+      image: '',
+      link: '',
+    },
+  }),
+  created() {
+    this.fillForm()
+  },
+  methods: {
+    submit() {
+      const { error: validationError } = ValidateKnowledge(this.form)
+
+      if (validationError) {
+        return this.$swal.fire({
+          icon: 'error',
+          title: validationError.name,
+          text: validationError.message,
+        })
+      }
+
+      if (this.initialValue?._id) {
+        this.$emit('update', this.form)
+      } else {
+        this.$emit('save', this.form)
+      }
+    },
+    fillForm() {
+      const formKeys = Object.keys(this.form) as Array<keyof typeof this.form>
+
+      for (const key of formKeys) {
+        if (key in this.initialValue) {
+          this.form[key] = this.initialValue[key]
+        }
+      }
+    },
+  },
+})
+</script>
+
+<template>
+  <form class="knowledge-form form" @submit.prevent="submit">
+    <DInput v-model="form.title" label="Title" type="text" />
+    <DInput v-model="form.image" label="Image link" type="text" />
+    <DInput v-model="form.link" label="Site link" type="text" />
+    <button class="submit">Save</button>
+  </form>
+</template>
+
+<style lang="scss"></style>
