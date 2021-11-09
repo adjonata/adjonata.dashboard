2c4561e04be00816df910088b8ddec0f01dc8bb0
adjonata <alexdjonata30@gmail.com>
2021-11-08 22:06:59 -0300

    feat: refactor - DList component

---
+++ components/DList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="d-list">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss">
+@import '~/styles/variables';
+
+.d-list {
+  width: 800px;
+  max-width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+
+  @media (max-width: $mobile) {
+    justify-content: space-around;
+  }
+
+  .d-item {
+    margin-bottom: 30px;
+    &.large {
+      width: 80%;
+    }
+    &.small {
+      width: 45%;
+    }
+  }
+}
+</style>
