fb26c87df2dd827d8d50fcab6bd277f32a6dd160
adjonata <djonataalex@gmail.com>
2022-02-27 01:23:48 -0300

    fix(bug): fixed a style issue in the modal

--- components/DModal.vue
+++ components/DModal.vue
@@ -39,7 +39,7 @@ export default Vue.extend({
 @import '~/styles/variables', '~/styles/flex';
 
 .modal {
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   background: rgba(0, 0, 0, 0.3);
